b77150496ecf59bae9d1fbddb2cb15f768258d21
卢展 <luz22@mails.tsinghua.edu.cn>
2023-09-27 20:55:27 +0800

    update: add hint for test_api

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -29,6 +29,8 @@ class APITestCase(TestCase):
     def test_register(self):
         """
         Example: 使用错误信息进行注册，检查返回值为失败
+        TODO: 这里的错误和返回码是在还没有实现注册参数校验的情况下的返回值，
+        在完成register_params_check后，你需要修改这里的错误信息和返回码
         """
         data = {"username": "123", "password": "21321"}
         response = self.client.post(
